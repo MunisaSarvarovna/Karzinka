8b751a74bb8152a250f0f41aae4c034db74a9753
Munisa Sarvarovna <munisasultonmurodova1@gmail.com>
2025-02-26 17:09:18 +0500

    yangi narsalar keldi

--- korzina.py
+++ korzina.py
@@ -3,4 +3,7 @@ from uuid import *
 
 a1=Korzinka("07:00 ochilashdi","23:00 yopiladi")
 a2=Joylashuv("6ta qator","5 ta filial","qizil va oq","sergili","1 gektar")
+a3=Product("gosh mahsulotlari, mevalar,sabzavotlar,oyinchoqlar,ichimliklar,yarimtayor mahsulotlar",
+           "tovuq mol qoy goshti,olma banan,nok apilsin mandarin","2027>l","100,54,73,34,23,12,56",
+           "input+=1")
 
